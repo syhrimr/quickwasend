9fa35ca045c8737aab2932205e2908c6c83cecd8
syhrimr <syahri.maulana@gmail.com>
2023-09-14 11:17:32 +0000

    fix: output type of devicecheck utils

--- utils/devicecheck.ts
+++ utils/devicecheck.ts
@@ -1,4 +1,8 @@
-export default function devicecheck(): boolean {
+type DeviceCheckOutput = {
+  isMobile: boolean;
+}
+
+export default function devicecheck(): DeviceCheckOutput {
   // the logic here is every device that has
   // less than 800x600 resolution considered as mobile
   const isMobile = ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 );
